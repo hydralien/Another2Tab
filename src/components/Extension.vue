614ff55243f01e64cff73a82d8d9234cae3baaa6
Boris Turchik <hydralien@gmail.com>
2020-12-25 17:53:54 +0100

    Extention management improvements

--- src/components/Extension.vue
+++ src/components/Extension.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="m-0 p-2 icon-wrapper">
-    <a v-bind:title="extension.name">
+    <a v-bind:title="extension.name" :class="[!extension.enabled ? 'disabled' : '']">
       <div class="p-2 row m-0 icon bookmark-item" v-on:click="navigate">
         <div class="py-0 pl-0 pr-1 col-2 icon-image-side">
           <img v-bind:src="iconUrl" v-bind:origin="extension.id" v-bind:alt="extension.name">
         </div>
         <div class="py-0 pl-1 pr-0 col-10 icon-name-side">
-          {{ extension.name }}
+          <span v-if="!extension.enabled">[DISABLED]</span> {{ extension.name }}
           <input type="hidden" v-bind:value="extension.id">
         </div>
       </div>
@@ -15,25 +15,35 @@
 </template>
 
 <script>
-	export default {
-		props: {
-			extension: Object,
-			settings: Object
-		},
-		computed: {
-			iconUrl: function () {
-				return "chrome://extension-icon/" + this.extension.id + "/128/1";
-			},
-		},
-		methods: {
-			navigate() {
-				if (this.extension.launchType) {
-					this.$chrome.management.launchApp(this.extension.id);
-				} else {
-					this.$chrome.tabs.create({url: this.extension.optionsUrl});
-				}
-				window.close();
-			}
-		}
-	};
+export default {
+  props: {
+    extension: Object,
+    settings: Object
+  },
+  computed: {
+    iconUrl: function () {
+      return "chrome://extension-icon/" + this.extension.id + "/128/1";
+    },
+  },
+  methods: {
+    navigate() {
+      if (!this.extension.enabled) return
+
+      if (this.extension.isApp) {
+        this.$chrome.management.launchApp(this.extension.id);
+      } else {
+        const extUrl = this.extension.optionsUrl || this.extension.homepageUrl
+        this.$chrome.tabs.create({url: extUrl});
+      }
+      // window.close();
+    }
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.disabled .bookmark-item {
+  background-color: lightgray;
+  cursor: default;
+}
+</style>
